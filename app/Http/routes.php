5a0f8bff6a24a7100a13d4095cd552404e6e0c5d
Alfirinel <dalnimdeluna@gmail.com>
2022-06-24 20:40:37 +0300

    view partial for errors display

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware'=>['auth']], function (){
+    Route::resource('task', 'TaskController', ['except'=>['show']]);
+});
+
